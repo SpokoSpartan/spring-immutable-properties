39289395802ee278f1b7ed24778bf6d6aa7a568e
SpokoSpartan <wojtekspoton@gmail.com>
2021-10-16 14:49:05 +0200

    Init simple Spring Boot application

---
+++ src/test/java/com/spot/on/properties/immutable/ImmutableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spot.on.properties.immutable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmutableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
